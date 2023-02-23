1d76e26ab017d7946ff2677b85fbc846ee29cfcb
codeX-pratik <superpratik9861@gmail.com>
2023-02-23 18:50:00 +0530

    java program

--- SEM_1/ASSIGNMENT/ASSIGNMENT_6/HW_2.java
+++ SEM_1/ASSIGNMENT/ASSIGNMENT_6/HW_2.java
@@ -1,15 +1,24 @@
 package ASSIGNMENT_6;
 import java.util.*;
+class Sphere {
+    void volume(double radius) {
+        double volume = 4.0/3 * Math.PI * Math.pow(radius , 3);
+        System.out.println("The volume of the Sphere is " + volume);
+    }
+}
+class Cuboid{
+    void volume(double length , double breadth , double height){
+        double volume = length * breadth * height;
+        System.out.println("The volume of the Cuboid is " + volume);
+    }
+}
+class Cube{
+    void volume (double side) {
+        double volume = Math.pow(side,3);
+        System.out.println("The volume of the cube is " + volume);
+    }
+}
 public class HW_2 {
-    public static double volumeSphere(double radius) {
-        return 4.0/3 * Math.PI * Math.pow(radius, 3);
-    }
-    public static double volumeCuboid(double length , double breath , double height) {
-        return length * breath * height;
-    }
-    public static double volumeCube(double side) {
-        return Math.pow(side, 3);
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the shape you want to find the volume : ");
@@ -17,25 +26,29 @@ public class HW_2 {
         System.out.print("Enter you choice (1-3) : ");
         int choice = sc.nextInt();
 
+        Sphere obj1 = new Sphere();
+        Cuboid obj2 = new Cuboid();
+        Cube obj3 = new Cube();
+
         switch(choice) {
             case 1 : 
                 System.out.print("Enter the radius of the sphere : ");
                 double radius = sc.nextDouble();
-                System.out.println("The volume of the sphere is " + volumeSphere(radius));
+                obj1.volume(radius);
             break;
             case 2 : 
                 System.out.print("Enter the length of the cuboid : ");
-                double lenght = sc.nextDouble();
+                double length = sc.nextDouble();
                 System.out.print("Enter the breath of the cuboid : ");
                 double breadth = sc.nextDouble();
                 System.out.print("Enter the heigth of the cuboid : ");
                 double height = sc.nextDouble();
-                System.out.println("The volume of the cubiod is " + volumeCuboid(lenght, breadth, height));
+                obj2.volume(length, breadth, height);
             break;
             case 3 : 
                 System.out.print("Enter the side of the cube : ");
                 double side = sc.nextDouble();
-                System.out.println("The volume of the cube is " + volumeCube(side));
+                obj3.volume(side);
             break;
             default : System.out.println("Invalid choice");
             break;
