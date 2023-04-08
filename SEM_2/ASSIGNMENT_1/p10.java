178184165a90b4db038efa1a7d6fbdd70273750a
codeX-pratik <superpratik9861@gmail.com>
2023-04-08 23:00:49 +0530

    java program

---
+++ SEM_2/ASSIGNMENT_1/p10.java
@@ -0,0 +1,35 @@
+package ASSIGNMENT_1;
+import java.util.*;
+public class p10 {
+    public static double sumColumn(double[][]m , int columnIndex) {
+        int r = m.length;
+        double sum = 0;
+        for (int i=0 ; i<r ; i++) {
+            sum += m[i][columnIndex];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Row and Column : ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        double [][]arr = new double[r][c];
+        System.out.print("Enter the Array row by row : ");
+        for (int i=0 ; i<r ; i++) {
+            for (int j=0 ;j<c ; j++) {
+                arr[i][j] = sc.nextDouble();
+            }
+        }
+        for (int i=0 ; i<r ; i++) {
+            for (int j=0 ; j<c ; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int i=0 ; i<c ; i++) {
+            System.out.println("Sum of the element at Column " + i + " is " + sumColumn(arr, i));
+        }
+        sc.close();
+    }
+}
---
+++ SEM_2/ASSIGNMENT_1/p4.java
@@ -0,0 +1,7 @@
+package ASSIGNMENT_1;
+
+public class p4 {
+    public static void main(String[] args) {
+        
+    }
+}
