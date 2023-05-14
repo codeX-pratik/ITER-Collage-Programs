919a03b8e84029df6a79509ae7b290277c530246
codeX-pratik <superpratik9861@gmail.com>
2023-05-14 15:50:51 +0530

    java program

--- SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_3/P4.java
+++ SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_3/P4.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class P4 {
+    public static < E > void printArray(E[] intputArray) {
+        for (int i=0 ; i<intputArray.length ; i++) {
+            System.out.print(intputArray[i] + "  ");
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the Array : ");
+        int size = sc.nextInt();
+
+        Integer IntegerArray[] = new Integer[size];
+        Double DoubleArray[] = new Double[size];
+
+        //Integer
+        System.out.println("Enter Array Elements");
+        for (int i=0 ; i<size ; i++) {
+            IntegerArray[i] = sc.nextInt();
+        }
+        System.out.println("Integer Array Contains :");
+        printArray(IntegerArray);
+        System.out.println();
+
+        //Double
+        System.out.println("Enter Array Element");
+        for(int i=0 ; i<size ; i++) {
+            DoubleArray[i] = sc.nextDouble();
+        }
+        System.out.println("Double Array contains :");
+        printArray(DoubleArray);
     }
 }
