0cec84dcd6d00202d765ab4f01423ec570612d37
codeX-pratik <superpratik9861@gmail.com>
2023-05-14 22:39:45 +0530

    java program

---
+++ SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_3/P5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class P5 {
+
+    public static <T> int count(T[]array , T item) {
+        int c = 0;
+        for (int i=0 ; i<array.length ; i++) {
+            if (item == array[i]) {
+                c++;
+            }
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in);
+        System.out.print("Enter the size of the array : ");
+        int size = sc.nextInt();
+
+        Integer E[] = new Integer[size];
+        System.out.println("Enter Array Element : ");
+        for (int i=0 ; i<size ; i++) {
+            E[i] = sc.nextInt();
+        }
+        System.out.print("Enter the element to search : ");
+        int item = sc.nextInt();
+
+        System.out.println("Number of times " + item + " present in the array is " + (count(E, item)));
+    }
+}
---
+++ SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_3/P6.java
@@ -0,0 +1,35 @@
+
+class Box<T> {
+    T data;
+    Box(T d) {
+        this.data = d;
+    }
+    public T getdata() {
+        return data;
+    }
+}
+
+public class P6 {
+    public static void main(String[] args) {
+        //String
+        Box<String> s1 = new Box<>("Hello world");
+        Box<String> s2 = new Box<>("Hello Everyone");
+
+        System.out.println(s1.getdata());
+        System.out.println(s2.getdata());
+        
+        //Integer
+        Box<Integer> I1 = new Box<>(34);
+        Box<Integer> I2 = new Box<>(95);
+
+        System.out.println(I1.getdata());
+        System.out.println(I2.getdata());
+
+        Box<Object> obj1 = new Box<>("Hello world");
+        Box<Object> obj2 = new Box<>(45);
+
+        System.out.println(obj1.getdata());
+        System.out.println(obj2.getdata());
+
+    }
+}
