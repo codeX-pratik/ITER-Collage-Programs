4818d6070ea321f6aab76c4ad3a68b229ebdfd74
codeX-pratik <superpratik9861@gmail.com>
2023-06-01 22:18:30 +0530

    java program

--- SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_3/HW_5.java
+++ SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_3/HW_5.java
@@ -1,36 +1,36 @@
 public class HW_5 {
     public static void main(String[] args) {
         int n = 4;
-        towerOfHanoi(n, "A (Scorce)", "B (Helper)", "C (Destination)");
+        towerOfHanoi(n, "A (Source)", "B (Helper)", "C (Destination)");
     }
 
-    public static void towerOfHanoi(int n , String Scorce , String Helper , String Destination) {
+    public static void towerOfHanoi(int n , String Source , String Helper , String Destination) {
         if (n == 1) {
-            System.out.println("Transfer disk " + n + " from " + Scorce + " to " + Destination);
+            System.out.println("Transfer disk " + n + " from " + Source + " to " + Destination);
             return;
         }
-        towerOfHanoi(n - 1, Scorce, Destination, Helper);
-        System.out.println("Transfer disk " + n + " from " + Scorce + " to " + Destination);
-        towerOfHanoi(n - 1 , Helper, Scorce, Destination);
+        towerOfHanoi(n - 1, Source, Destination, Helper);
+        System.out.println("Transfer disk " + n + " from " + Source + " to " + Destination);
+        towerOfHanoi(n - 1 , Helper, Source, Destination);
     }
 }
 
 /*
 
-Transfer disk 1 from A (Scorce) to B (Helper)
-Transfer disk 2 from A (Scorce) to C (Destination)
+Transfer disk 1 from A (Source) to B (Helper)
+Transfer disk 2 from A (Source) to C (Destination)
 Transfer disk 1 from B (Helper) to C (Destination)
-Transfer disk 3 from A (Scorce) to B (Helper)
-Transfer disk 1 from C (Destination) to A (Scorce)
+Transfer disk 3 from A (Source) to B (Helper)
+Transfer disk 1 from C (Destination) to A (Source)
 Transfer disk 2 from C (Destination) to B (Helper)
-Transfer disk 1 from A (Scorce) to B (Helper)
-Transfer disk 4 from A (Scorce) to C (Destination)
+Transfer disk 1 from A (Source) to B (Helper)
+Transfer disk 4 from A (Source) to C (Destination)
 Transfer disk 1 from B (Helper) to C (Destination)
-Transfer disk 2 from B (Helper) to A (Scorce)
-Transfer disk 1 from C (Destination) to A (Scorce)
+Transfer disk 2 from B (Helper) to A (Source)
+Transfer disk 1 from C (Destination) to A (Source)
 Transfer disk 3 from B (Helper) to C (Destination)
-Transfer disk 1 from A (Scorce) to B (Helper)
-Transfer disk 2 from A (Scorce) to C (Destination)
+Transfer disk 1 from A (Source) to B (Helper)
+Transfer disk 2 from A (Source) to C (Destination)
 Transfer disk 1 from B (Helper) to C (Destination)
 
  */
