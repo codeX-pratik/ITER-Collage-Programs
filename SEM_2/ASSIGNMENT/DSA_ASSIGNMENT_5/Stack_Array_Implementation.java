ee7c9312f130c2286fa5ee7853a7229e7ab866bd
codeX-pratik <superpratik9861@gmail.com>
2023-06-24 17:50:42 +0530

    java program

---
+++ SEM_2/ASSIGNMENT/DSA_ASSIGNMENT_5/Stack_Array_Implementation.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Stack_Array_Implementation{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Object[] s = new Object[MAX];
+        int top = -1;
+
+        while (true) {
+            System.out.println("---> MENY <---");
+            System.out.println("0. Exit...");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Display");
+            System.out.print("Enter your Choice : ");
+            int choice = sc.nextInt();
+
+            switch(choice) {
+                case 0 : System.exit(0); break;
+                case 1 : top = push(s, top); break;
+                case 2 : top = pop(s, top); break;
+                case 3 : display(s, top); break;
+                default : System.out.println("Invalid Choice!!"); break;
+            }
+        }
+    }
+
+    public static final int MAX = 10;
+
+    public static boolean isEmpty(int top) {
+        return (top == -1);
+    }
+    public static boolean isFull(int top) {
+        return top == MAX - 1;
+    }
+
+    public static int push(Object[] S , int top) {
+        Scanner sc = new Scanner(System.in);
+        if (isFull(top)) {
+            System.out.println("The stack is Full !!");
+        } else {
+            System.out.print("Enter the element : ");
+            Object e = (Object) sc.next();
+            S[++top] = e;
+        }
+        return top;
+    }
+
+    public static int pop(Object[] S , int top) {
+        if (isEmpty(top)) {
+            System.out.println("The Stack is Empty !!");
+        } else {
+            System.out.println("The " + S[top--] + " element is Poped");
+        }
+        return top;
+    }
+
+    public static void display(Object[] S , int top) {
+        if (isEmpty(top)) {
+            System.out.println("The Stack is Empty !!");
+        } else {
+            System.out.println("The stack Elements are : ");
+            for (int i= top ; i>=0 ; i--) {
+                System.out.println(S[i]);
+            }
+        }
+    }
+}
