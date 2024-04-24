1eede84c809e927ffdbd76fd91aa22aec82939eb
codeX-pratik <superpratik9861@gmail.com>
2024-04-25 01:00:32 +0530

    MWDW-2

---
+++ SEM_4/MWDW-2/ASSIGNMENT-4/p9 (More clearly)/List_of_music.js
@@ -0,0 +1,25 @@
+let musicList = [
+    "2023 Recap northernelg edit",
+    "Ace of Base - Happy Nation (Official Music Video)",
+    "Akon- Right Now (slowedreverb)",
+    "Isabel LaRosa - favorite (english)",
+    "Leat'eq - Tokyo  (Official Music Video HD)",
+    "Lindsey Stirling - Sleigh Ride (Audio)",
+    "Lukas Graham - 7 Years [OFFICIAL LYRIC VIDEO]",
+    "Miki Matsubara - Stay With Me HD (Club Mix)",
+    "One Direction - Night Changes",
+    "Phao - 2 Phut Hon (KAIZ Remix)  TikTok Vietnamese Music 2020"
+];
+
+// let musicList = {
+//     1: "2023 Recap northernelg edit",
+//     2: "Ace of Base - Happy Nation (Official Music Video)",
+//     3: "Akon- Right Now (slowedreverb)",
+//     4: "Isabel LaRosa - favorite (english)",
+//     5: "Leat'eq - Tokyo  (Official Music Video HD)",
+//     6: "Lindsey Stirling - Sleigh Ride (Audio)",
+//     7: "Lukas Graham - 7 Years [OFFICIAL LYRIC VIDEO]",
+//     8: "Miki Matsubara - Stay With Me HD (Club Mix)",
+//     9: "One Direction - Night Changes",
+//     10: "Phao - 2 Phut Hon (KAIZ Remix)  TikTok Vietnamese Music 2020",
+// }
---
+++ SEM_4/MWDW-2/ASSIGNMENT-4/p9 (More clearly)/script.js
@@ -0,0 +1,96 @@
+let currentSong = 0;
+let music = document.getElementById("mySong");
+let musicName = document.getElementById("musicName");
+let progressed = document.getElementById("progressed");
+let progressBar = document.getElementById("progressBar");
+document.getElementById("musicName").innerText = `${musicList[currentSong]}`;
+window.onload = time;
+
+// Getting the time in proper format
+function getTime(time) {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = '0' + String(min);
+    }
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = '0' + String(sec);
+    }
+    return min + ":" + sec;
+}
+
+// Setting the time and the progress bar
+function time() {
+    music.ontimeupdate = (e) => {
+        document.getElementById("currentTime").innerHTML = getTime(music.currentTime);
+        progressed.style.width = Math.floor(music.currentTime * 100 / music.duration) + "%";
+    };
+
+    progressBar.onclick = (e) => {
+        music.currentTime = ((e.offsetX / progressBar.offsetWidth) * music.duration);
+    }
+    
+    setTimeout(() => {
+        document.getElementById("duration").innerHTML = getTime(music.duration);
+        
+    },200);
+}
+
+// Play and Pause button function
+function playAndPause() {
+    if (music.paused) {
+        pauseButton();
+        time();
+        music.play();
+
+    } else {
+        playButton();
+        time();
+        music.pause();
+    }
+}
+
+// Pause Button
+function pauseButton() {
+    let playPause = document.getElementById("play_pause").classList;
+    playPause.remove("fa-play");
+    playPause.add("fa-pause");
+}
+
+// Play Button
+function playButton() {
+    let playPause = document.getElementById("play_pause").classList;
+    playPause.remove("fa-pause");
+    playPause.add("fa-play");
+}
+
+// Next song Button function
+function nextSong() {
+    currentSong--;
+    if (currentSong < 0) {
+        currentSong = musicList.length - 1;
+    }
+    music.src = `../Audio/${musicList[currentSong]}.mp3`;
+    musicName.innerText = `${musicList[currentSong]}`;
+    pauseButton();
+    time();
+    music.play();
+}
+
+// Previous Song Button function
+function previousSong() {
+    currentSong++;
+    if (currentSong >= musicList.length) {
+        currentSong = 0;
+    }
+    music.src = `../Audio/${musicList[currentSong]}.mp3`;
+    musicName.innerText = `${musicList[currentSong]}`;
+    pauseButton();
+    time();
+    music.play();
+}
+
+// Events
+document.getElementById("play_pause").addEventListener("click", playAndPause);
+document.getElementById("playNext").addEventListener("click", previousSong);
+document.getElementById("playPrevious").addEventListener("click", nextSong);
